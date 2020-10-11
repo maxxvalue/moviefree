23a4b947b0787f19f600163f2092ba69ec3de2c7
maxxvalue <32428979+maxxvalue@users.noreply.github.com>
2020-10-11 22:35:34 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-//$pass = "999999";
-$dbname = "movie";
+$host = "https://databases-auth.000webhost.com/index.php";
+$user = "id15097840_movie";
+$pass = "ols%brUJxRc6FT*g";
+$dbname = "id15097840_mv";
 $con = mysqli_connect($host, $user, $pass, $dbname);
  mysqli_query($con,"set  char set utf8 ");
 
